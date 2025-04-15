ac73db1c8679ea1e1d7e84a4ec685282f0511a08
Menno Drescher <menno@cbaconsult.eu>
2025-04-15 15:25:45 +0200

    fix: Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,6 +94,9 @@ const config = {
         card: "0 1px 3px rgba(0, 0, 0, 0.05), 0 1px 2px rgba(0, 0, 0, 0.1)",
         elevated: "0 10px 15px -3px rgba(0, 0, 0, 0.08), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
       },
+      borderColor: {
+        border: 'hsl(var(--border))'
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
